7e527c5369012a86c183674d2bc275c01a72fad4
Amber Pratt <78624822+apratt7891@users.noreply.github.com>
2021-05-31 11:13:48 -0600

    start prompts

--- Modules/tracker/index.js
+++ Modules/tracker/index.js
@@ -5,7 +5,31 @@ const inquirer = require('inquirer');
 // link to generateHTML for index.html//
 const generateHTML= require('./src/generateHTML.js');
 
+// team array//
+const teamArray = [];
 
+//options prompt//
+const trackerOptions = () => {
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'option',
+            message: "What would you like to do?",
+            choices: [
+                'View All Departments',
+                'View All Employees',
+                'View all Roles',
+                'Add a Department',
+                'Add a Role',
+                'Add an Employee',
+                'Update an Employee Role',
+                ]
+        }
+    ])
+    .then(answers) => {
+        
+    }
+}
 
 
 fs.writeFile('index.html', generateHTML(name, github), err => {
