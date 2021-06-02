9b52174ccc7972567cf6a0449b7f9b6b02ef7a33
Amber Pratt <78624822+apratt7891@users.noreply.github.com>
2021-06-01 19:22:18 -0600

    corrections

---
+++ Modules/tracker/db/connection.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql");
+
+
+// Connect to database
+const db = mysql.createConnection({
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'password',
+    database: 'employee'
+});
+
+
+
+
+
+module.exports = db;
